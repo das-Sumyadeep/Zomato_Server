35bdd61340b78bab904b8389b4a77170a7bd0a6e
das-Sumyadeep <133164855+das-Sumyadeep@users.noreply.github.com>
2023-11-29 21:40:24 +0530

    Update index.js

--- API/Auth/index.js
+++ API/Auth/index.js
@@ -99,9 +99,8 @@ Router.get('/google/callback', passport.authenticate('google', { failureRedirect
     (req, res) => {
 
         try {
-
             if (req.user && req.user.token) {
-                console.log('google login');
+                console.log("google login");
                 res.cookie('jwt', req.user.token, { httpOnly: false, maxAge: 24 * 60 * 60, secure: false });
                 res.redirect('https://only-zomato-master.netlify.app');
 
